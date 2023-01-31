613d4e9518780f729b3f57b1f197b0e5e86b286d
ayzee_cheema <ayzee820@gmail.com>
2023-01-31 15:07:34 +0500

    appointment and image upload

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\Watermark::class,
+        // Commands\Watermark::class,
     ];
     /**
      * Define the application's command schedule.
@@ -23,8 +23,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('image:watermark')
-            ->everyMinute();
+        // $schedule->command('image:watermark')
+        //     ->everyMinute();
     }
     /**
      * Register the commands for the application.
